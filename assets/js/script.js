4c0f1cd595b4b757646124f4413f5f70bfaf5f2a
Jody M <jodyamm@hotmail.com>
2023-05-18 11:27:32 +0000

    Clean-up: Install ext Prettier and tidy up code before merge

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,26 @@
 // initialize kaboom context
 kaboom({
-    global: true,
-    width: 800,
-    height: 800,
-    canvas: document.querySelector("#game"),
-    scale: 1,
-    debug: true,
-    background: [0, 0, 0, 0],
-  })
-  
-  // loads sprite
-  loadRoot("assets/");
-  loadSprite('yoda1', 'sprites/yoda-1.png');
+  global: true,
+  width: 800,
+  height: 800,
+  canvas: document.querySelector("#game"),
+  scale: 1,
+  debug: true,
+  background: [0, 0, 0, 0],
+});
 
+// loads sprite
+loadRoot("assets/");
+loadSprite("yoda1", "sprites/yoda-1.png");
 
-  
-  // create game scenes
-  scene("game", () => {
+// create game scenes
+scene("game", () => {
+  const yoda1 = add([sprite("yoda1"), pos(400, 200), scale(0.1)]);
 
-    const yoda1 = add([
-        sprite("yoda1"),
-        pos(400, 200),
-        scale(0.1),
-      ]);
+  //layers
+  layers(["bg", "obj", "ui"], "obj");
+  add([sprite, layer("obj")]);
+});
 
-    
-  
-    //layers
-    layers(['bg', 'obj', 'ui'], 'obj')
-    add([sprite, layer("obj")]);
-
-  
-  
-  })
-  
-  // start game
-  go("game")
+// start game
+go("game");
