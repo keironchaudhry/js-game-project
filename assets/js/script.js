a2f1d31556a25868b8c805fefffeae2ebf4482ad
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-05-19 11:49:24 +0200

    Feat: Creation of life containers + js logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,9 @@ kaboom({
   background: [0, 0, 0, 0],
 });
 
+// global variables
+let lives = 3;
+
 // loads sprite
 loadRoot("assets/");
 loadSprite("yoda1", "sprites/yoda-1.png");
@@ -21,6 +24,33 @@ scene("game", () => {
   layers(["bg", "obj", "ui"], "obj");
   add([sprite, layer("obj")]);
 });
+
+// create life containers
+function createLifeIcons() {
+  if (lifeContainer.children.length >= 3) {
+    return;
+  }
+
+  const iconsToAdd = Math.min(lives, 3 - lifeContainer.children.length);
+  for (let i = 0; i < iconsToAdd; i++) {
+    const lifeIcon = document.createElement("div");
+    lifeIcon.classList.add("life-icon");
+    lifeContainer.appendChild(lifeIcon);
+  }
+}
+
+function removeLifeIcon() {
+  const lifeIcons = lifeContainer.getElementsByClassName("life-icon");
+  if (lifeIcons.length > 0) {
+    lifeContainer.removeChild(lifeIcons[lifeIcons.length - 1]);
+  }
+  if (lifeIcons.length <= 0) {
+    // If lives end up at 0, gameOver function called
+  }
+}
+
+// calls function to create life container(s)
+createLifeIcons();
 
 // start game
 go("game");
