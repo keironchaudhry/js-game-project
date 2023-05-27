3503b6842cc0f99525c2fb1b3c53d7faa29a88cf
Jody M <jodyamm@hotmail.com>
2023-05-27 17:36:12 +0000

    Fix issue: html was loading at the bottom of the screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,8 @@
 
 kaboom({
   global: true,
-  width: 800,
-  height: 800,
+  width: 400,
+  height: 400,
   canvas: document.querySelector("#game"),
   scale: 1,
   debug: true,
@@ -16,6 +16,7 @@ let lives = 3;
 // loads sprite
 loadRoot("assets/");
 loadSprite("yoda1", "sprites/yoda-1.png");
+loadSprite("ground", "background/ground.png");
 
 
 
@@ -27,11 +28,21 @@ scene("game", () => {
     fixed(),
     z(100),
   ])
+
+  const bg = add([
+    fixed(),
+    z(1),
+  ])
   
   // This will be on top, because the parent node has z(100)
   ui.add([
     sprite("yoda1"),
     scale(0.2),
+  ])
+  bg.add([
+    sprite("ground"),
+    scale(0.2),
+    pos(0, 40)
   ])
   
 });
