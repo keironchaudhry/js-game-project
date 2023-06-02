eced3468717d878321fa4f2cc0b338a6dd368d0d
Jody M <jodyamm@hotmail.com>
2023-06-02 09:35:51 +0000

    Add: Jump and sprite reset for landing

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,16 +13,30 @@ kaboom({
 // global variables
 let lives = 3;
 
-const SPEED = 250;
+const SPEED = 150;
 const JUMP_FORCE = 240;
 
 setGravity(640);
 
-const spriteNames = ["yoda1", "yoda2", "yoda3"];
+const spriteNames = [
+  "yoda1",
+  "yoda2",
+  "yoda3",
+  "yoda4",
+  "yoda5",
+  "yoda6",
+  "yoda7",
+  "yoda8",
+];
 const spritePaths = [
   "/sprites/yoda-1.png",
   "/sprites/yoda-2.png",
-  "/sprites/yoda-2.png",
+  "/sprites/yoda-3.png",
+  "/sprites/yoda-4.png",
+  "/sprites/yoda-5.png",
+  "/sprites/yoda-6.png",
+  "/sprites/yoda-7.png",
+  "/sprites/yoda-8.png",
 ];
 
 spriteNames.forEach((name, index) => {
@@ -34,8 +48,14 @@ loadRoot("assets/");
 loadSprite("yoda1", "sprites/yoda-1.png");
 loadSprite("yoda2", "sprites/yoda-2.png");
 loadSprite("yoda3", "sprites/yoda-3.png");
-loadSprite("yodaStop", "sprites/yoda-halt.png");
 loadSprite("yoda4", "sprites/yoda-4.png");
+loadSprite("yoda5", "sprites/yoda-5.png");
+loadSprite("yoda6", "sprites/yoda-6.png");
+loadSprite("yoda7", "sprites/yoda-7.png");
+loadSprite("yoda8", "sprites/yoda-8.png");
+loadSprite("jump", "sprites/jump.png");
+
+loadSprite("yodaStop", "sprites/yoda-halt.png");
 loadSprite("ground", "background/ground.png");
 
 // create game scenes
@@ -58,7 +78,7 @@ scene("game", () => {
       tiles: {
         "=": () => [
           sprite("ground"),
-          area({ scale: 0.9 }),
+          area({ scale: 0.77 }),
           pos(-60, 350),
           body({ isStatic: true }),
           anchor("bot"),
@@ -68,8 +88,8 @@ scene("game", () => {
   );
 
   const yoda1 = add([
-    sprite("yoda1", { flipX: true }),
-    scale(0.1),
+    sprite("yoda1"),
+    scale(0.2),
     anchor("bot"),
     area(),
 
@@ -77,10 +97,10 @@ scene("game", () => {
     pos(100, 20),
   ]);
   let currentSpriteIndex = 0;
-
+  const spriteChangeDelay = 1;
   // Movements
 
-  onKeyDown("right", () => {
+  onKeyDown("right", async () => {
     currentSpriteIndex++;
 
     if (currentSpriteIndex >= spriteNames.length) {
@@ -89,52 +109,40 @@ scene("game", () => {
     const nextSpriteName = spriteNames[currentSpriteIndex];
     yoda1.move(SPEED, 0), (yoda1.flipX = false);
     yoda1.use(sprite(nextSpriteName));
+    await wait(spriteChangeDelay);
   });
 
-  onKeyDown("right", () => {
+  onKeyDown("left", async () => {
     currentSpriteIndex++;
 
     if (currentSpriteIndex >= spriteNames.length) {
       currentSpriteIndex = 0;
     }
     const nextSpriteName = spriteNames[currentSpriteIndex];
+    yoda1.move(-SPEED, 0);
+
     yoda1.use(sprite(nextSpriteName));
-    yoda1.flipX = true; 
-    yoda1.move(SPEED, 0);
+    yoda1.flipX = true;
+    await wait(spriteChangeDelay);
   });
 
-  onKeyDown("left", () => {
-    currentSpriteIndex++;
+  const defaultSpriteName = "default";
+  const jumpSpriteName = "jump";
 
-    if (currentSpriteIndex >= spriteNames.length) {
-      currentSpriteIndex = 0;
-    }
-    const nextSpriteName = spriteNames[currentSpriteIndex];
-    yoda1.move(-SPEED, 0), (yoda1.flipX = true);
-    yoda1.use(sprite(nextSpriteName));
+  onKeyDown("up", () => {
+    // Trigger the jump action
+    yoda1.move(0, -SPEED);
+    yoda1.use(sprite(jumpSpriteName));
   });
 
-  // onKeyDown("left", () => {
-  //   yoda1.move(-SPEED, 0), (yoda1.flipX = true);
-  //   yoda1.use(sprite("yoda4", "yoda3", "yoda2"));
-  // });
+  onKeyRelease("up", () => {
+    // Reset the sprite to the default one
+    yoda1.use(sprite("yoda1"));
+  });
 
-  // onKeyDown("right", () => {
-  //   yoda1.move(SPEED, 0), (yoda1.flipX = true);
-  //   yoda1.use(sprite("yoda4", "yoda3", "yoda2"));
-  // });
-
-  // onKeyDown("up", () => {
-  //   yoda1.move(0, -SPEED);
-  // });
-
-  // onKeyDown("down", () => {
-  //   yoda1.move(0, SPEED);
-  // });
-
-  // onClick(() => {
-  //   yoda1.moveTo(mousePos());
-  // });
+  onClick(() => {
+    yoda1.moveTo(mousePos());
+  });
 
   // ui.add([sprite("yoda1"), scale(0.2)]);
   // bg.add([sprite("ground"), scale(0.2), pos(0, 40)]);
